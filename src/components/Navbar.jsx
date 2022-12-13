638eaba022bb427a94a408f2cbddfb91439be79f
Upeksha Herath <upekshadilshan000@gmail.com>
2022-12-13 23:06:44 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+    return (
+        <div>
+           <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));   // help to catch the root component 
+
+root.render(<App />); // render the content to the web page
